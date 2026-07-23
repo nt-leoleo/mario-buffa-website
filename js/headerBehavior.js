76e6bf34431c2690fdd4b3b3f719ae65ed74b64a
nt-leoleo <pederneraleonardo729@gmail.com>
2026-07-23 17:05:37 -0300

    starting carrusel

---
+++ js/headerBehavior.js
@@ -0,0 +1,22 @@
+const header = document.querySelector("header");
+
+let headerHeight;
+function updateHeaderHeight() {
+    headerHeight = header.offsetHeight;
+
+    document.documentElement.style.setProperty(
+        "--header-height",
+        `${headerHeight}px`
+    );
+}
+
+updateHeaderHeight();
+
+window.addEventListener("resize", updateHeaderHeight);
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= headerHeight) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
