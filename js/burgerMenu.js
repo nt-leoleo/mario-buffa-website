df0f7c6a116a882d176d260a98aa8737078334b4
nt-leoleo <pederneraleonardo729@gmail.com>
2026-08-05 15:40:43 -0300

    starting footer

--- js/burgerMenu.js
+++ js/burgerMenu.js
@@ -1,11 +1,14 @@
 const burgerMenu = document.querySelector('.burger-menu');
 const mHeader = document.querySelector('header');
+const pHeader = document.querySelector('.pre-header');
 
 function openMenu() {
-    mHeader.classList.add('disabled')
+    mHeader.classList.add('disabled');
+    pHeader.classList.add('disabled');
     burgerMenu.classList.remove('inactive');
 }
 function closeMenu() {
     burgerMenu.classList.add('inactive');
     mHeader.classList.remove('disabled');
+    pHeader.classList.remove('disabled');
 }
---
+++ js/components.js
@@ -0,0 +1,11 @@
+async function loadComponents() {
+    const components = document.querySelectorAll('[data-component]');
+
+    for (const component of components) {
+        const file = component.dataset.component;
+        const response = await fetch(file);
+        component.innerHTML = await response.text();
+    }
+}
+
+loadComponents();
