2ccbb33e466df541ea7a07954b38d4cd61226f40
nt-leoleo <pederneraleonardo729@gmail.com>
2026-08-28 16:57:36 -0300

    finishing modals

--- js/products.js
+++ js/products.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector("#productModal");
 
 const modalImage = document.querySelector("#modalImage");
+const modalCategory = document.querySelector("#modalCategory");
 const modalName = document.querySelector("#modalName");
 const modalDescription = document.querySelector("#modalDescription");
+const modalCharacteristics = document.querySelector("#modalCharacteristics");
 const modalWhatsapp = document.querySelector("#modalWhatsapp");
 
 const leftImageButton = document.querySelector("#leftImage");
@@ -93,6 +95,8 @@ fetch("./data/products.json")
             card.addEventListener("click", () => {
 
                 const product = products[card.id];
+                const currentDescriptiveParagraphs = Object.values(product.description);
+                const currentCharacteristics = Object.values(product.characteristics);
 
                 currentImages = Object.values(product.images);
                 imagePosition = 0;
@@ -100,17 +104,47 @@ fetch("./data/products.json")
                 indicatorContainer.innerHTML = "";
 
                 currentImages.forEach(() => {
-
                     const dot = document.createElement("div");
-
                     dot.classList.add("dot");
-
                     indicatorContainer.appendChild(dot);
                 });
 
+                modalCategory.textContent = product.category;
                 modalName.textContent = product.name;
-                modalDescription.textContent = product.description;
                 modalImage.alt = product.name;
+
+                modalDescription.innerHTML = "";
+                currentDescriptiveParagraphs.forEach((text) => {
+                    const paragraph = document.createElement("p");
+                    paragraph.classList.add('paragraph');
+                    paragraph.textContent = text;
+                    modalDescription.appendChild(paragraph);
+                });
+
+                modalCharacteristics.innerHTML = "";
+                currentCharacteristics.forEach((characteristic) => {
+                    const feature = document.createElement('div');
+                    const svgIco = document.createElement('img');
+                    const title = document.createElement('h3');
+                    const description = document.createElement('p');
+                    const titleDescription = document.createElement('div');
+                    feature.classList.add('modalFeature');
+                    svgIco.classList.add('modalFeatureSvgIco')
+                    title.classList.add('modalFeatureTitle');
+                    description.classList.add('modalFeatureDescription')
+                    titleDescription.classList.add('modalFeatureTitleDescription');
+
+                    svgIco.src = characteristic.svg;
+                    svgIco.alt = characteristic.title;
+                    title.textContent = characteristic.title;
+                    description.textContent = characteristic.description;
+
+                    feature.appendChild(svgIco);
+                    titleDescription.appendChild(title);
+                    titleDescription.appendChild(description);
+                    feature.appendChild(titleDescription);
+                    modalCharacteristics.appendChild(feature);
+                });
 
                 const message =
                     `Hola, quisiera consultar por el producto ${product.name}.`;
